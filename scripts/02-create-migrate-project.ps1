6bfeda1eb75298d9e3c26a42632d4e2aa33e33ab
Donovan McCoy <mccoy.donov@gmail.com>
2026-02-18 13:48:28 -0500

    messing with private endpoints templates

--- scripts/02-create-migrate-project.ps1
+++ scripts/02-create-migrate-project.ps1
@@ -6,9 +6,20 @@ $ProjectName = "Test-CMF-HyperV-Project"
 $Location = "centralus"
 $ResourceGroupName = "rg-Azure-Migrate"
 $ApiVersion = "2020-06-01-preview"
+$ConnectivityMethod = "PrivateEndpoint"
+$SubscriptionId = (Get-AzContext).Subscription.Id
+$VNetName = "vnet-hub"
+$SubnetName = "private-endpoints"
 
-Write-Host "Creating Resource Group: $ResourceGroupName in $Location" -ForegroundColor Yellow
-New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Force -Verbose 
+if ($ConnectivityMethod -eq "PrivateEndpoint") {
+    $TemplateFile = "../templates/azure-migrate-project-private-endpoints.json"
+    $ProjectName = "$ProjectName-Private"
+    Write-Host "Creating Azure Migrate Project: $ResourceGroupName -> $ProjectName in $Location" -ForegroundColor Yellow 
+    New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -projectName $ProjectName -location $Location -vnetResourceId "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.Network/virtualNetworks/$VNetName" -subnetResourceId "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.Network/virtualNetworks/$VNetName/subnets/$SubnetName" -ApiVersion $ApiVersion -Verbose
+} else {
+    $TemplateFile = "../templates/azure-migrate-project-public-endpoint.json" 
+    $ProjectName = "$ProjectName-Public"
+    Write-Host "Creating Azure Migrate Project: $ResourceGroupName -> $ProjectName in $Location" -ForegroundColor Yellow
+    New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -projectName $ProjectName -location $Location -ApiVersion $ApiVersion -Verbose
+}
 
-Write-Host "Creating Azure Migrate Project: $ResourceGroupName -> $ProjectName in $Location" -ForegroundColor Yellow
-New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile ../templates/azure-migrate-project.json -projectName $ProjectName -location $Location -ApiVersion $ApiVersion -Verbose
